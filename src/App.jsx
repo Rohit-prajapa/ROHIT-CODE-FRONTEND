c9b3c946f6100b3f988458ef308a9ef6e0b6b9ce
RohitDprajapati <rahulprajapati69424@gmail.com>
2026-09-05 18:03:08 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import SettingsPanel from "./components/SettingsPanel";
 const SOCKET_URL = "https://rohit-code-backend.onrender.com";
 const AI_API_URL = "https://rohit-code-backend.onrender.com/api/ai/generate";
 const EXECUTION_API_URL = "https://rohit-code-execution-service.onrender.com";
-const EXECUTION_API_URL = "http://localhost:10001";
+// const EXECUTION_API_URL = "http://localhost:10001";
 
 
 const STORAGE_KEY = "rohit-code-project";
